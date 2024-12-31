a1b73fcee3c30395bd6287b549504007975647ec
Jayesh Gaur <tdc9@animehaven.fun>
2024-12-31 09:54:35 +0530

    revisiontime

--- basics/basic.js
+++ basics/basic.js
@@ -1,11 +1,11 @@
-console.log(`Hello`);
-console.log(`I like pizza!`);
+console.log(`Hello World`);
+console.log(`I like cats!`);
 
 window.alert(`This is an alert!`);
-window.alert(`I like pizza!`);
+window.alert(`I like cats!`);
 
 document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza!`;
+document.getElementById("myP").textContent = `I like cats!`;
 
 // This is a comment
 
